7adbd6932a2dcc1e28eeff27050b0e2220e5bb6f
Tirumala-Sai-Teja <80871045+Tirumala-Sai-Teja@users.noreply.github.com>
2021-04-28 20:57:42 +0530

    Create PlusMinus.py

---
+++ PlusMinus.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    l=len(arr)
+    p=0
+    n=0
+    z=0
+    for i in arr:
+        if i>0:
+            p+=1
+        elif i<0:
+            n+=1
+        else:
+            z+=1
+    print(p/l)
+    print(n/l)
+    print(z/l)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
